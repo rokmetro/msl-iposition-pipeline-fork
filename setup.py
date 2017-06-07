1a6b1677b110a4b46026562f66592c541ac9d31f
kevroy314 <kevin.horecka@gmail.com>
2017-06-06 22:37:15 -0500

    fixed import

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     author_email='kevin.horecka@gmail.com',
     maintainer='Kevin Horecka',
     # License details
-    license=read(os.path.abspath('./LICENSE.txt')),
+    license=read(path.abspath('./LICENSE.txt')),
     include_package_data=True,
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
