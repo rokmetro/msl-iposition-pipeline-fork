7a5ad1b294a542951da6f94a1be0078410a1e97b
kevroy314 <kevin.horecka@gmail.com>
2017-06-06 22:20:25 -0500

    Changed config

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,11 @@ sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../cogrecon'))
 
+import mock
+ 
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 
 # -- General configuration ------------------------------------------------
 
