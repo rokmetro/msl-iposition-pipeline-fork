c08fb90f603615d4fa8e969d60df6b1754702ca6
kevroy314 <kevin.horecka@gmail.com>
2018-01-08 20:55:00 -0600

    v0.1.7 - CB Crossing and FD/Lacunarity
    -Added context boundary crossing metric to summarize_navigation_data
    -Added time, space, and spacetime fractal dimension and lacunarity to summarize_navigation_data
    -Added helper functions for context boundary crossings, fractal dimension, and lacunarity to time_travel_task_analytics (note, they've been written to be generalizable to a variety of different used cases and may deserve to be integrated into .tools)
    -Updated/finished FD_Lacunarity.ipynb which demos the function
    -Updated Time_Travel_Task_Analyses.ipynb to include new analysis methods
    -Updated Time_Travel_Task_Analyses.ipynb visualizer to allow for variable subplot shapes

--- cogrecon/core/data_flexing/time_travel_task/time_travel_task_analytics.py
+++ cogrecon/core/data_flexing/time_travel_task/time_travel_task_analytics.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 import easygui
 import logging
+import scipy.optimize
 
 
 # example search_directory: r'C:\Users\Kevin\Desktop\Work\Time Travel Task\v2'
@@ -89,7 +90,8 @@ def summarize_test_data(search_directory=None, file_regex="\d\d\d_\d_2_\d_\d\d\d
 
 
 def summarize_navigation_data(search_directory=None, file_regex="\d\d\d_\d_2_\d_\d\d\d\d-\d\d-\d\d_\d\d-\d\d-\d\d.dat",
-                              output_path='time_travel_task_navigation_summary.csv', last_pilot_id=20, verbose=True):
+                              output_path='time_travel_task_navigation_summary.csv', last_pilot_id=20, verbose=True,
+                              temporal_boundary_regions=None):
     """
     This function will produce an output file which contains the navigation summary data for all data files in a
     directory, searching recursively based on a regular expression.
@@ -99,6 +101,11 @@ def summarize_navigation_data(search_directory=None, file_regex="\d\d\d_\d_2_\d_
     :param output_path: the output path to save the data (CSV format)
     :param last_pilot_id: for the 'pilot?' column, IDs less than or equal to last_pilot_id will be marked True
     :param verbose: If True, participant information will be logged to logging.info as parsing progresses.
+    :param temporal_boundary_regions: the (Nr, Nd, 2) representation of the boundary regions in the environment where
+    Nr is the number of regions (note that regions can intersect and do not have to cover the whole space - boundaries
+    are defined as when the contained regions for a point change in any way), Nd is the number of dimensions, and 2
+    are the min and max of the box region in each dimension for each region. The default is
+    [[[-100, 15]], [[15, 30]], [[30, 45]], [[45, 100]]].
 
     :rtype: bool
     :return: True if files were successfully processed, False otherwise.
@@ -111,6 +118,9 @@ def summarize_navigation_data(search_directory=None, file_regex="\d\d\d_\d_2_\d_
     if not os.path.exists(search_directory):
         raise IOError('Specified search directory was not found.')
 
+    if temporal_boundary_regions is None:
+        temporal_boundary_regions = [[[-100, 15]], [[15, 30]], [[30, 45]], [[45, 100]]]
+
     files = find_data_files_in_directory(search_directory, file_regex=file_regex)
 
     if len(files) == 0:
@@ -119,18 +129,29 @@ def summarize_navigation_data(search_directory=None, file_regex="\d\d\d_\d_2_\d_
 
     fp = open(output_path, 'wb')
 
-    header = 'subID,trial,inverse,datetime,pilot?,total_time,space_travelled,time_travelled,space_time_travelled'
+    header = 'subID,trial,inverse,datetime,pilot?,total_time,space_travelled,time_travelled,space_time_travelled,' + \
+             'context_boundary_crossings,fd_time,fd_space,fd_spacetime,' + \
+             'lacunarity_time,lacunarity_space,lacunarity_spacetime'
 
     fp.write(header + '\r\n')
 
     for path in files:
         iterations = read_binary_file(path)
         total_time, space_travelled, time_travelled, space_time_travelled = get_exploration_metrics(iterations)
+        timeline = [[i['time_val']] for i in iterations]
+        spaceline = [[i['x'], i['z']] for i in iterations]
+        spacetimeline = [[i['x'], i['z'], i['time_val']] for i in iterations]
+        boundary_crossings = count_boundary_crossings(timeline, temporal_boundary_regions)
+        fd_t, lac_t = calculate_fd_and_lacunarity(timeline)
+        fd_s, lac_s = calculate_fd_and_lacunarity(spaceline)
+        fd_st, lac_st = calculate_fd_and_lacunarity(spacetimeline)
         meta = get_filename_meta_data(os.path.basename(path))
         line_start = meta['subID'] + ',' + meta['trial'] + ',' + meta['inverse'] + ',' + str(meta['datetime'])
         line_start += ',' + str(int(meta['subID']) <= last_pilot_id)
         line = line_start + ',' + str(total_time) + ',' + str(space_travelled) + ',' \
-                          + str(time_travelled) + ',' + str(space_time_travelled)
+                          + str(time_travelled) + ',' + str(space_time_travelled) + ',' \
+                          + str(boundary_crossings) + ',' + str(fd_t) + ',' + str(fd_s) + ',' + str(fd_st) + ',' \
+                          + str(lac_t) + ',' + str(lac_s) + ',' + str(lac_st)
         if verbose:
             logging.info(line_start)
         fp.write(line + '\r\n')
@@ -139,3 +160,46 @@ def summarize_navigation_data(search_directory=None, file_regex="\d\d\d_\d_2_\d_
     fp.close()
 
     return True
+
+
+def get_regions_of_points(point, regions):
+    in_regions = []
+    for region in regions:
+        in_regions.append(all([r[0] < p <= r[1] for p, r in zip(point, region)]))
+    return in_regions
+
+
+def count_boundary_crossings(path_points, regions):
+    count = 0
+    prev_regions = get_regions_of_points(path_points[0], regions)
+    for p in path_points:
+        current_regions = get_regions_of_points(p, regions)
+        count += prev_regions != current_regions
+        prev_regions = current_regions
+    return count
+
+
+def count_boxes(data, scale):
+    boxed_path = np.floor(np.divide(data, scale))
+    unique = np.unique(boxed_path, axis=0)
+    filled_boxes = len(unique)
+    return filled_boxes
+
+
+def calculate_fd_and_lacunarity(data):
+    scale_range = 20
+    r = np.array([2.0 ** (scale_range / 2) / (2.0 ** i) for i in range(scale_range, 0, -1)])  # Powers of 2 around 0
+    N = [count_boxes(data, ri) for ri in r]
+    Nlog = np.log(N)
+    ste = np.std(Nlog) / np.sqrt(len(data))
+    indicies = [idx for idx, n in enumerate(Nlog) if (not n <= (min(Nlog) + ste) and not n >= (max(Nlog) - ste))]
+    N = np.take(N, indicies)
+    r = np.take(r, indicies)
+
+    def linear_function(x, A, Df):
+        return Df * x + A
+
+    popt, pcov = scipy.optimize.curve_fit(linear_function, np.log(1. / r), np.log(N))
+    lacunarity, fd = popt
+
+    return fd, lacunarity
